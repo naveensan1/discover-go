5d64b9bb6713f6d5516d3b9100872d722a519133
Naveen Sangapala <naveensan1@gmail.com>
2016-03-22 00:11:16 -0700

    Reduced struct

--- rest-client/struct.go
+++ rest-client/struct.go
@@ -4,29 +4,12 @@ package main
 type Movie struct {
 	Title      string
 	Year       string
-	Rated      string
-	Released   string
-	Runtime    string
-	Genre      string
-	Director   string
-	Writer     string
-	Actors     string
-	Plot       string
-	Language   string
-	Country    string
-	Awards     string
-	Poster     string
-	Metascore  string
 	ImdbRating string
 	ImdbVotes  string
 	ImdbID     string
-	Type       string
-	Response   string
 }
 
 //MovieQuery - Search Results
 type MovieQuery struct {
-	Search       []Movie
-	Response     string
-	totalResults string
+	Search []Movie
 }
